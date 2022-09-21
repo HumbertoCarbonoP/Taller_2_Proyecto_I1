5a761175509fee16c2f65d3d79174e87c6df98f1
Humberto Carbonó P <hacarbonop@eafit.edu.co>
2022-09-21 16:53:50 -0500

    Agrego el taller 2.

---
+++ mlappproject/petsclassiffier/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from petsclassiffier.models import mlModels
+
+# Register your models here.
+admin.site.register(mlModels)
---
+++ mlappproject/petsclassiffier/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PetsclassiffierConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'petsclassiffier'
